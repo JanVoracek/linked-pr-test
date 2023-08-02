e998683af925c4815da430e65889f2596ac841dc
Jan Voracek <jan@voracek.net>
2023-08-02 14:58:48 +0200

    Test

--- .github/workflows/link-pr/action.ts
+++ .github/workflows/link-pr/action.ts
@@ -5,4 +5,4 @@ import fs from 'fs';
 const eventPath = process.env.GITHUB_EVENT_PATH;
 const prEvent = JSON.parse(fs.readFileSync(eventPath, 'utf8'));
 
-console.log(prEvent);
+console.log(github.context);
