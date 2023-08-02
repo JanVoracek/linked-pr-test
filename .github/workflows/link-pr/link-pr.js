85265d5c0b6965b85dbb80c4ba848d413165e108
Jan Voracek <jan@voracek.net>
2023-08-02 14:12:41 +0200

    Read PR from file

--- .github/workflows/link-pr/link-pr.js
+++ .github/workflows/link-pr/link-pr.js
@@ -1,1 +1,8 @@
-console.log('Hello world');
+const core = require('@actions/core');
+const github = require('@actions/github');
+const fs = require('fs');
+
+const eventPath = process.env.GITHUB_EVENT_PATH;
+const prEvent = JSON.parse(fs.readFileSync(eventPath, 'utf8'));
+
+console.log(prEvent);
