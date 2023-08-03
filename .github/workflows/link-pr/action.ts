18fdb056ba658b068dec317929dadddd6976d920
Jan Voracek <jan@voracek.net>
2023-08-03 10:42:27 +0200

    Refactor

--- .github/workflows/link-pr/action.ts
+++ .github/workflows/link-pr/action.ts
@@ -1,15 +1,9 @@
 import github from '@actions/github';
-import { stripIndent } from 'common-tags';
+import { IssuePullRequestLinker } from './issue-pull-request-linker';
 
 const pr = github.context.payload.pull_request!;
 const changes = github.context.payload.changes;
 
-const linkedPrsHeader = stripIndent`
-    <!-- Do not edit manually after this comment -->
-    ---
-
-    **Linked PRs:**
-`.replaceAll('\n', '\r\n');
 
 // https://regex101.com/r/fecV0x/1
 const issueReferencePattern = /^Issue: #(\d+)\s*$/m;
@@ -27,64 +21,12 @@ if (issueNumber === previousIssueNumber) {
 }
 
 const octokit = github.getOctokit(process.env.GITHUB_TOKEN!);
-
-type IssueBody = { description: string; linkedPrs: Set<number> };
-
-function parseIssueBody(issueBody: string): IssueBody {
-  const linkedPrsHeaderIndex = issueBody.lastIndexOf(linkedPrsHeader);
-
-  if (linkedPrsHeaderIndex === -1) {
-    return {
-      description: issueBody,
-      linkedPrs: new Set(),
-    };
-  }
-
-  const description = issueBody.slice(0, linkedPrsHeaderIndex).trimEnd();
-  const linkedPrsList = issueBody.slice(linkedPrsHeaderIndex + linkedPrsHeader.length);
-
-  // https://regex101.com/r/QxqEIk/1
-  const linkedPrs = [...linkedPrsList.matchAll(/^- PR #(\d+)/gm)].map(match => parseInt(match[1]!, 10));
-
-  return {
-    description,
-    linkedPrs: new Set(linkedPrs),
-  };
-}
-
-function formatIssueBody(issue: IssueBody): string {
-    const sortedPrs = [...issue.linkedPrs].sort((a, b) => a - b);
-    const linkedPrs = sortedPrs.map(pr => `- PR #${pr}`).join('\r\n');
-  
-    if (sortedPrs.length === 0) {
-      return issue.description;
-    }
-  
-    return `${issue.description}\r\n\r\n${linkedPrsHeader}\r\n\r\n${linkedPrs}`;
-}
-
-async function updateLinkedPrs(issueNumber: number, prNumber: number, operation: 'add' | 'delete') {
-  const response = await octokit.rest.issues.get({
-    issue_number: issueNumber,
-    owner: github.context.repo.owner,
-    repo: github.context.repo.repo,
-  });
-
-  const issueBody = parseIssueBody(response.data.body ?? '');
-  issueBody.linkedPrs[operation](prNumber);
-
-  await octokit.rest.issues.update({
-    issue_number: issueNumber,
-    owner: github.context.repo.owner,
-    repo: github.context.repo.repo,
-    body: formatIssueBody(issueBody),
-  });
-}
+const issuePrLinker = new IssuePullRequestLinker(octokit, github.context.repo);
 
 if (previousIssueNumber) {
-  await updateLinkedPrs(previousIssueNumber, pr.number, 'delete');
+  await issuePrLinker.updateLinkedPullRequestsForIssue(previousIssueNumber, pr.number, 'delete');
 }
 
 if (issueNumber) {
-  await updateLinkedPrs(issueNumber, pr.number, 'add');
+  await issuePrLinker.updateLinkedPullRequestsForIssue(issueNumber, pr.number, 'add');
 }
---
+++ .github/workflows/link-pr/issue-pull-request-linker.ts
@@ -0,0 +1,69 @@
+import { stripIndent } from 'common-tags';
+import type github from '@actions/github';
+
+export type IssueBody = { description: string; linkedPrs: Set<number> };
+
+type Octokit = ReturnType<typeof github.getOctokit>;
+type Repo = typeof github.context.repo;
+
+const linkedPrsHeader = stripIndent`
+    <!-- Do not edit manually after this comment -->
+    ---
+
+    **Linked PRs:**
+`.replaceAll('\n', '\r\n');
+
+export class IssuePullRequestLinker {
+  constructor(private octokit: Octokit, private repo: Repo) {}
+
+  async updateLinkedPullRequestsForIssue(issueNumber: number, prNumber: number, operation: 'add' | 'delete') {
+    const response = await this.octokit.rest.issues.get({
+      issue_number: issueNumber,
+      owner: this.repo.owner,
+      repo: this.repo.repo,
+    });
+
+    const issueBody = parseIssueBody(response.data.body ?? '');
+    issueBody.linkedPrs[operation](prNumber);
+
+    await this.octokit.rest.issues.update({
+      issue_number: issueNumber,
+      owner: this.repo.owner,
+      repo: this.repo.repo,
+      body: formatIssueBody(issueBody),
+    });
+  }
+}
+
+function parseIssueBody(issueBody: string): IssueBody {
+  const linkedPrsHeaderIndex = issueBody.lastIndexOf(linkedPrsHeader);
+
+  if (linkedPrsHeaderIndex === -1) {
+    return {
+      description: issueBody,
+      linkedPrs: new Set(),
+    };
+  }
+
+  const description = issueBody.slice(0, linkedPrsHeaderIndex).trimEnd();
+  const linkedPrsList = issueBody.slice(linkedPrsHeaderIndex + linkedPrsHeader.length);
+
+  // https://regex101.com/r/QxqEIk/1
+  const linkedPrs = [...linkedPrsList.matchAll(/^- PR #(\d+)/gm)].map(match => parseInt(match[1]!, 10));
+
+  return {
+    description,
+    linkedPrs: new Set(linkedPrs),
+  };
+}
+
+function formatIssueBody(issue: IssueBody): string {
+  const sortedPrs = [...issue.linkedPrs].sort((a, b) => a - b);
+  const linkedPrs = sortedPrs.map(pr => `- PR #${pr}`).join('\r\n');
+
+  if (sortedPrs.length === 0) {
+    return issue.description;
+  }
+
+  return `${issue.description}\r\n\r\n${linkedPrsHeader}\r\n\r\n${linkedPrs}`;
+}
