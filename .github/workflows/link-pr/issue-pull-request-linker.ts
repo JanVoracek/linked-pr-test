6486afe0713685436e1da93277dad14e97a553b0
Jan Voracek <jan@voracek.net>
2024-02-19 12:29:14 +0100

    Log response

--- .github/workflows/link-pr/issue-pull-request-linker.ts
+++ .github/workflows/link-pr/issue-pull-request-linker.ts
@@ -1,6 +1,6 @@
-import type github from 'npm:@actions/github@6.0.0';
-import { RequestError } from 'npm:@octokit/request-error@5.0.1';
-import { parseIssueBody, formatIssueBody } from './issue-body-parser.ts';
+import type github from "npm:@actions/github@6.0.0";
+import { RequestError } from "npm:@octokit/request-error@5.0.1";
+import { parseIssueBody, formatIssueBody } from "./issue-body-parser.ts";
 
 type Octokit = ReturnType<typeof github.getOctokit>;
 type Repo = typeof github.context.repo;
@@ -8,30 +8,37 @@ type Repo = typeof github.context.repo;
 export class IssuePullRequestLinker {
   constructor(private octokit: Octokit, private repo: Repo) {}
 
-  async updateLinkedPullRequestsForIssue(issueNumber: number, prNumber: number, operation: 'add' | 'delete') {
+  async updateLinkedPullRequestsForIssue(
+    issueNumber: number,
+    prNumber: number,
+    operation: "add" | "delete"
+  ) {
     try {
-    const response = await this.octokit.rest.issues.get({
-      issue_number: issueNumber,
-      owner: this.repo.owner,
-      repo: this.repo.repo,
-    });
+      const response = await this.octokit.rest.issues.get({
+        issue_number: issueNumber,
+        owner: this.repo.owner,
+        repo: this.repo.repo,
+      });
 
-    const issueBody = parseIssueBody(response.data.body ?? '');
-    const lastModified = response.headers['last-modified'];
+      const issueBody = parseIssueBody(response.data.body ?? "");
+      const lastModified = response.headers["last-modified"];
 
-    issueBody.linkedPrs[operation](prNumber);
+      issueBody.linkedPrs[operation](prNumber);
 
-    await this.octokit.rest.issues.update({
-      issue_number: issueNumber,
-      owner: this.repo.owner,
-      repo: this.repo.repo,
-      headers: {
-        'If-Unmodified-Since': lastModified,
-      },
-      body: formatIssueBody(issueBody),
-    });
+      await this.octokit.rest.issues.update({
+        issue_number: issueNumber,
+        owner: this.repo.owner,
+        repo: this.repo.repo,
+        headers: {
+          "If-Unmodified-Since": lastModified,
+        },
+        body: formatIssueBody(issueBody),
+      });
     } catch (error) {
       console.error(error);
+      if (error instanceof RequestError) {
+        console.error(error.response);
+      }
       throw error;
     }
   }
